5d8a0e968492ff7fca7f444052e8d5eced0c5630
Dan Albert <dan@gingerhq.net>
2011-11-30 20:20:03 -0800

    Added the files I forgot...

---
+++ sock.c
@@ -0,0 +1,54 @@
+/**
+ * @file sock.c
+ * @author Dan Albert
+ * @date Created 11/30/2011
+ * @date Last updated 11/30/2011
+ * @version 0.1
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ * @section DESCRIPTION
+ *
+ * Defines functions for use with sockets implementation.
+ *
+ */
+#include <arpa/inet.h>
+#include "sock.h"
+
+int sock_connect(char *host) {
+	struct sockaddr_in addr;
+	int fd;
+
+	fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd == -1) {
+		perror("Unable to create socket");
+		return -1;
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	inet_pton(AF_INET, host, &addr.sin_addr);
+	addr.sin_port = htons(SERVER_PORT);
+
+	if (connect(fd, (struct sockadd *)&addr, sizeof(addr)) == -1) {
+		perror("Unable to connect to server");
+		return -1;
+	}
+
+	return fd;
+}
---
+++ sock.h
@@ -0,0 +1,38 @@
+/**
+ * @file sock.h
+ * @author Dan Albert
+ * @date Created 11/30/2011
+ * @date Last updated 11/30/2011
+ * @version 0.1
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ * @section DESCRIPTION
+ *
+ * Declares functions for use with sockets implementation.
+ *
+ */
+#ifndef SOCK_H
+#define SOCK_H
+
+/// Port the server will listen on
+#define SERVER_PORT 10054
+
+int sock_connect(char *host);
+
+#endif // SOCK_H
